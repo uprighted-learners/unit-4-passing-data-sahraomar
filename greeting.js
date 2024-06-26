5d15b8f4697b2cd3dd86c9ec25b33ff492c181b0
sahraomar <sahraaliwrites@gmail.com>
2024-06-26 14:26:35 -0400

    commit2

--- greeting.js
+++ greeting.js
@@ -1,1 +1,13 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const greetingElement = document.querySelector("#friend-greeting");
+
+  const params = new URLSearchParams(document.location.search);
+  const friendName = params.get("name");
+  // logic to show change in greeting
+  if (friendName) {
+    greetingElement.textContent = `Hi ${friendName}!`;
+  } else {
+    greetingElement.textContent = `hello ${friendName}!`;
+  }
+});
--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const formElement = document.getElementById("name-form");
+  const inputElement = document.getElementById("user-input");
+  const responseElement = document.querySelector("#computer-response"); // for "go away!"
+
+  formElement.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const userName = inputElement.value.trim().toLowerCase();
+    const enemies = ["plankton", "moe", "larry"]; // Add your enemies' names here in lowercase
+
+    if (enemies.includes(userName)) {
+      responseElement.textContent = "Go away!";
+      responseElement.style.color = "red"; // Emphasize the enemy response
+    } else {
+      document.location =
+        "/greeting.html?name=" + encodeURIComponent(inputElement.value.trim());
+    }
+
+    inputElement.value = ""; // Clear the input field
+  });
+});
